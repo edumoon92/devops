d8357e88305b2213a00f115dd7e17ca5702da871
henry <106457765+edumoon92@users.noreply.github.com>
2023-03-16 23:32:19 +0900

    add saucedemo

---
+++ docker/node-mysql-app/saucedemo/src/components/HeaderContainer.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './HeaderContainer.css';
+import DrawerMenu from './DrawerMenu';
+import CartButton from './CartButton';
+
+const HeaderContainer = ({
+  customClass,
+  secondaryLeftComponent,
+  secondaryRightComponent,
+  secondaryTitle,
+}) => {
+  /**
+   * @TODO:
+   * This can't be tested yet because enzyme currently doesn't support ReactJS17,
+   * see https://github.com/enzymejs/enzyme/issues/2429.
+   * This means we can't fully mount the component and test all rendered components
+   * and functions
+   */
+  /* istanbul ignore next */
+  const LeftComponent = ({ leftComponent }) => (
+    <div className="left_component">{React.cloneElement(leftComponent)}</div>
+  );
+  /**
+   * @TODO:
+   * This can't be tested yet because enzyme currently doesn't support ReactJS17,
+   * see https://github.com/enzymejs/enzyme/issues/2429.
+   * This means we can't fully mount the component and test all rendered components
+   * and functions
+   */
+  /* istanbul ignore next */
+  const RightComponent = ({ rightComponent }) => (
+    <div className="right_component">{React.cloneElement(rightComponent)}</div>
+  );
+  /**
+   * @TODO:
+   * This can't be tested yet because enzyme currently doesn't support ReactJS17,
+   * see https://github.com/enzymejs/enzyme/issues/2429.
+   * This means we can't fully mount the component and test all rendered components
+   * and functions
+   */
+  /* istanbul ignore next */
+  const Title = ({ title }) => <span className="title">{title}</span>;
+  const extraClass = customClass ? ` ${customClass}` : '';
+
+  return (
+    <div id="header_container" className={`header_container${extraClass}`}>
+      <div className="primary_header">
+        <div id="menu_button_container">
+          <DrawerMenu />
+        </div>
+        <div className="header_label">
+          <div className="app_logo">Swag Labs</div>
+        </div>
+        <div id="shopping_cart_container" className="shopping_cart_container">
+          <CartButton />
+        </div>
+      </div>
+      <div className="header_secondary_container">
+        {secondaryLeftComponent && (
+          <LeftComponent leftComponent={secondaryLeftComponent} />
+        )}
+        {secondaryTitle && <Title title={secondaryTitle} />}
+        {secondaryRightComponent && (
+          <RightComponent rightComponent={secondaryRightComponent} />
+        )}
+      </div>
+    </div>
+  );
+};
+HeaderContainer.propTypes = {
+  /**
+   * A custom class for the header container
+   */
+  customClass: PropTypes.string,
+  /**
+   * A react component for the left
+   */
+  secondaryLeftComponent: PropTypes.element,
+  /**
+   * A react component for the right
+   */
+  secondaryRightComponent: PropTypes.element,
+  /**
+   * A title
+   */
+  title: PropTypes.string,
+};
+HeaderContainer.defaultProps = {
+  customClass: undefined,
+  secondaryLeftComponent: undefined,
+  secondaryRightComponent: undefined,
+  secondaryTitle: undefined,
+};
+
+export default HeaderContainer;
---
+++ docker/node-mysql-app/saucedemo/src/pages/Finish.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Checkmark from '../assets/img/checkmark.png';
+import SwagLabsFooter from '../components/Footer';
+import HeaderContainer from '../components/HeaderContainer';
+import PropTypes from 'prop-types';
+import Button, { BUTTON_SIZES } from '../components/Button';
+import { ROUTES } from '../utils/Constants';
+import './Finish.css';
+
+const Finish = ({ history }) => {
+  return (
+    <div id="page_wrapper" className="page_wrapper">
+      <div id="contents_wrapper">
+        <HeaderContainer secondaryTitle="Checkout: Complete!" />
+        <div
+          id="checkout_complete_container"
+          className="checkout_complete_container"
+        >
+          <img alt="Pony Express" className="pony_express" src={Checkmark} />
+          <h2 className="complete-header">Thank you for your order!</h2>
+          <div className="complete-text">
+            Your order has been dispatched, and will arrive just as fast as the
+            pony can get there!
+          </div>
+          <Button
+            label="Back Home"
+            onClick={() => history.push(ROUTES.INVENTORY)}
+            size={BUTTON_SIZES.SMALL}
+            testId="back-to-products"
+          />
+        </div>
+      </div>
+      <SwagLabsFooter />
+    </div>
+  );
+};
+Finish.propTypes = {
+  /**
+   * The history
+   */
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default withRouter(Finish);
