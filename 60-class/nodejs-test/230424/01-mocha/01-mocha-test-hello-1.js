a76f0c2cddd81dc15539cb19dcda808d01bce377
henry <106457765+edumoon92@users.noreply.github.com>
2023-03-27 07:48:17 +0000

    Create 01-mocha-test-hello-1.js

---
+++ 60-class/nodejs-test/230424/01-mocha/01-mocha-test-hello-1.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+describe('#Hello World!', function () {
+    it('입력 값은 Hello World!', function () {
+        var input = 'Hello World!'; // 입력 값이라고 가정
+
+        assert.equal('Hello World', input);
+
+        console.log('Hello World Ok');
+    });
+});
