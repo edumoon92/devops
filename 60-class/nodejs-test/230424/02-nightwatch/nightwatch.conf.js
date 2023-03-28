ffd188a9def6d6de48a143f395f0a3349781d0ea
henry <106457765+edumoon92@users.noreply.github.com>
2023-03-28 17:43:56 +0900

    Update nightwatch.conf.js

--- 60-class/nodejs-test/230424/02-nightwatch/nightwatch.conf.js
+++ 60-class/nodejs-test/230424/02-nightwatch/nightwatch.conf.js
@@ -10,10 +10,10 @@ module.exports = {
         "host" : "127.0.0.1",
         "port" : 9515,
         "cli_args" : {
-            "webdriver.chrome.driver" : "./selenium/chromedriver.exe",
-            "webdriver.gecko.driver" : "./selenium/geckodriver.exe",
-            "webdriver.ie.driver" : "./selenium/IEDriverServer_Win32_3.14.0.exe",
-            "webdriver.edge.driver" : "./selenium/MicrosoftWebDriver.exe"
+            //"webdriver.chrome.driver" : "./selenium/chromedriver.exe",
+            //"webdriver.gecko.driver" : "./selenium/geckodriver.exe",
+            //"webdriver.ie.driver" : "./selenium/IEDriverServer_Win32_3.14.0.exe",
+            //"webdriver.edge.driver" : "./selenium/MicrosoftWebDriver.exe"
          }
     },
     
