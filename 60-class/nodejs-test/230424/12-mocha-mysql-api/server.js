1e10e96a5d49795b33f90677eb1e979f6593fccf
henry <106457765+edumoon92@users.noreply.github.com>
2023-03-29 00:30:21 +0900

    db env added

--- 60-class/nodejs-test/230424/12-mocha-mysql-api/server.js
+++ 60-class/nodejs-test/230424/12-mocha-mysql-api/server.js
@@ -8,9 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const database = mysql.createConnection({
-    user: 'henry',
-    password: 'tta123!@',
-    database: 'testdb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.get('/init', (req, res) => {
