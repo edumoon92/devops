6539b944966e07e865034f251a32e47a6cb77639
henry <106457765+edumoon92@users.noreply.github.com>
2023-04-10 22:01:09 +0900

    Update entrypoint.sh

--- 70-appium/70-android-sdk/build-docker-android-sdk/vnc/entrypoint.sh
+++ 70-appium/70-android-sdk/build-docker-android-sdk/vnc/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Check if VNC server is already running
+if [ -f /tmp/.X1-lock ]; then
+    echo "VNC server is already running, stopping it..."
+    /usr/bin/vncserver -kill :1
+fi
+
 # Start the VNC server
 /usr/bin/vncserver :1 -geometry 1280x800 -depth 24
 
