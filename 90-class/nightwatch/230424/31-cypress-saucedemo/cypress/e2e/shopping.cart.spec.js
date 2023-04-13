cadd82a6e43de3e17f27a025abc9da326b438ba5
henry <106457765+edumoon92@users.noreply.github.com>
2023-04-13 15:59:23 +0900

    changed

---
+++ 90-class/nightwatch/230424/31-cypress-saucedemo/cypress/e2e/shopping.cart.spec.js
@@ -0,0 +1,39 @@
+describe('Sauce Labs Demo App', () => {
+  beforeEach(() => {
+    cy.login('standard_user', 'secret_sauce');
+  });
+
+  it('should add item to cart and see the cart badge', () => {
+    cy.get('.inventory_item')
+      .first()
+      .find('.btn_primary.btn_inventory')
+      .click();
+
+    cy.get('.shopping_cart_badge')
+      .should('have.text', '1');
+  });
+
+  it('should remove item from cart and see the cart badge disappear', () => {
+    cy.get('.inventory_item')
+      .first()
+      .find('.btn_primary.btn_inventory')
+      .click();
+
+    cy.get('.shopping_cart_badge')
+      .should('have.text', '1');
+
+    cy.get('.shopping_cart_link')
+      .click();
+
+    cy.get('.cart_item')
+      .first()
+      .find('.btn_secondary.cart_button')
+      .click();
+
+    cy.get('.cart_item')
+      .should('have.length', 0);
+
+    cy.get('.shopping_cart_badge')
+      .should('not.exist');
+  });
+});
