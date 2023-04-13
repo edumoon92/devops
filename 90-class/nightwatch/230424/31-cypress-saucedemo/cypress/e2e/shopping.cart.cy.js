9eda64739e429aff012e315760631e441637127c
henry <106457765+edumoon92@users.noreply.github.com>
2023-04-13 16:09:07 +0900

    Update shopping.cart.cy.js

--- 90-class/nightwatch/230424/31-cypress-saucedemo/cypress/e2e/shopping.cart.cy.js
+++ 90-class/nightwatch/230424/31-cypress-saucedemo/cypress/e2e/shopping.cart.cy.js
@@ -1,7 +1,11 @@
 describe('Sauce Labs Demo App', () => {
   beforeEach(() => {
     cy.visit('http://192.168.56.101:3000/');
-    cy.login('standard_user', 'secret_sauce');
+
+    // Log in with standard user credentials
+    cy.get('[data-test=username]').type('standard_user');
+    cy.get('[data-test=password]').type('secret_sauce');
+    cy.get('[data-test=login-button]').click();
   });
 
   it('should add item to cart and see the cart badge', () => {
