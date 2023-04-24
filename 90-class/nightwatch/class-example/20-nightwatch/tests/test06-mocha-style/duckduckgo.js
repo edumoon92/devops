6a8fae726e88cc865ade3b00f05e33419fc971c2
henry <106457765+edumoon92@users.noreply.github.com>
2023-04-24 23:22:58 +0900

    Update duckduckgo.js

--- 90-class/nightwatch/class-example/20-nightwatch/tests/test06-mocha-style/duckduckgo.js
+++ 90-class/nightwatch/class-example/20-nightwatch/tests/test06-mocha-style/duckduckgo.js
@@ -1,7 +1,7 @@
 describe('duckduckgo example', function() {
   it('Search Nightwatch.js and check results', function(browser) {
     browser
-      .navigateTo('https://duckduckgo.com')
+      .url('https://duckduckgo.com')
       .waitForElementVisible('#search_form_input_homepage')
       .sendKeys('#search_form_input_homepage', ['Nightwatch.js'])
       .click('#search_button_homepage')
