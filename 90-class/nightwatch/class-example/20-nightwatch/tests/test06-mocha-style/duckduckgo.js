17f6185c680ebd43b20f241dce31caddf8d7ce34
henry <106457765+edumoon92@users.noreply.github.com>
2023-04-24 23:46:33 +0900

    Update duckduckgo.js

--- 90-class/nightwatch/class-example/20-nightwatch/tests/test06-mocha-style/duckduckgo.js
+++ 90-class/nightwatch/class-example/20-nightwatch/tests/test06-mocha-style/duckduckgo.js
@@ -1,11 +1,10 @@
-describe('duckduckgo example', function() {
+describe('ecosia example', function() {
   it('Search Nightwatch.js and check results', function(browser) {
     browser
-      .url('https://duckduckgo.com')
-      .waitForElementVisible('#search_form_input_homepage')
-      .sendKeys('#search_form_input_homepage', ['Nightwatch.js'])
-      .click('#search_button_homepage')
-      .assert.visible('.results--main')
-      .assert.textContains('.results--main', 'Nightwatch.js');
+      .url('https://www.ecosia.org/')
+      .setValue('input[type=search]', 'nightwatch')
+      .click('button[type=submit]')
+      .assert.containsText('#main', 'Nightwatch.js')
+      .end();
   }); 
 });
