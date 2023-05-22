0a0def7054911ea3c95ebf1b462c96e0b692565d
henry <106457765+edumoon92@users.noreply.github.com>
2023-05-22 21:01:06 +0900

    Update test-xcalc.py

--- 90-class/robotframework/230522/day2/additional/02-mouse-test/test-xcalc.py
+++ 90-class/robotframework/230522/day2/additional/02-mouse-test/test-xcalc.py
@@ -1,15 +1,18 @@
 import pyautogui
+import subprocess
 import time
 
 # Open xcalc (replace with your calculator application if necessary)
 #pyautogui.press('super')  # Press Alt key to open the application menu
-time.sleep(0.5)  # Wait for the menu to open
-pyautogui.typewrite('xcalc')  # Type 'xcalc' to search for the calculator
-time.sleep(0.5)  # Wait for the search results to appear
-pyautogui.press('enter')  # Press Enter to open xcalc
+#time.sleep(0.5)  # Wait for the menu to open
+#pyautogui.typewrite('xcalc')  # Type 'xcalc' to search for the calculator
+#time.sleep(0.5)  # Wait for the search results to appear
+#pyautogui.press('enter')  # Press Enter to open xcalc
+
+subprocess.Popen(['xcalc'])  # Replace with the appropriate command to open xcalc
 
 # Wait for xcalc to open (you can adjust the sleep duration as needed)
-time.sleep(10)
+time.sleep(2)
 
 # Perform a simple calculation
 pyautogui.press('2')  # Press 2
@@ -17,7 +20,7 @@ pyautogui.press('+')  # Press +
 pyautogui.press('3')  # Press 3
 pyautogui.press('=')  # Press = to perform the calculation
 
-time.sleep(11)
+time.sleep(3)
 # Get the result
 result_location = pyautogui.locateOnScreen('result.png')  # Replace 'result.png' with the actual image of the result
 if result_location is not None:
