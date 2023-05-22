8e66b903f7e7ea3d6445b160bb38bac0a944459e
henry <106457765+edumoon92@users.noreply.github.com>
2023-05-22 21:24:49 +0900

    Update test-xcalc-capture.py

--- 90-class/robotframework/230522/day2/additional/02-mouse-test/test-xcalc-capture.py
+++ 90-class/robotframework/230522/day2/additional/02-mouse-test/test-xcalc-capture.py
@@ -5,7 +5,7 @@ import time
 subprocess.Popen(['xcalc'])  # Replace with the appropriate command to open xcalc
 
 # Wait for xcalc to open (you can adjust the sleep duration as needed)
-time.sleep(2)
+time.sleep(1)
 
 # Perform a simple calculation
 pyautogui.press('2')  # Press 2
@@ -13,10 +13,10 @@ pyautogui.press('+')  # Press +
 pyautogui.press('3')  # Press 3
 pyautogui.press('=')  # Press = to perform the calculation
 
-time.sleep(3)
+time.sleep(1)
 
 #im2 = pyautogui.screenshot('my_screenshot.png')
-im3 = pyautogui.screenshot('result.png', region=(0, 0, 50, 100))
+im3 = pyautogui.screenshot('result.png', region=(30, 10, 200, 80))
 
 # Close xcalc
 pyautogui.hotkey('alt', 'f4')  # Press Alt+F4 to close the application
