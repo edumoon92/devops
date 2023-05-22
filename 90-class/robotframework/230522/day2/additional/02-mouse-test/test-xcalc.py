9c22fa20f55051cef5ce96f63220f70042b78dab
henry <106457765+edumoon92@users.noreply.github.com>
2023-05-22 20:44:50 +0900

    Create test-xcalc.py

---
+++ 90-class/robotframework/230522/day2/additional/02-mouse-test/test-xcalc.py
@@ -0,0 +1,31 @@
+import pyautogui
+import time
+
+# Open xcalc (replace with your calculator application if necessary)
+pyautogui.press('alt')  # Press Alt key to open the application menu
+time.sleep(0.5)  # Wait for the menu to open
+pyautogui.typewrite('xcalc')  # Type 'xcalc' to search for the calculator
+time.sleep(0.5)  # Wait for the search results to appear
+pyautogui.press('enter')  # Press Enter to open xcalc
+
+# Wait for xcalc to open (you can adjust the sleep duration as needed)
+time.sleep(2)
+
+# Perform a simple calculation
+pyautogui.press('2')  # Press 2
+pyautogui.press('+')  # Press +
+pyautogui.press('3')  # Press 3
+pyautogui.press('=')  # Press = to perform the calculation
+
+# Get the result
+result_location = pyautogui.locateOnScreen('result.png')  # Replace 'result.png' with the actual image of the result
+if result_location is not None:
+    result_center = pyautogui.center(result_location)
+    pyautogui.click(result_center)  # Click on the result to select it
+    selected_text = pyautogui.hotkey('ctrl', 'c')  # Copy the selected text to the clipboard
+    print("Result:", selected_text)
+else:
+    print("Result not found.")
+
+# Close xcalc
+pyautogui.hotkey('alt', 'f4')  # Press Alt+F4 to close the application
